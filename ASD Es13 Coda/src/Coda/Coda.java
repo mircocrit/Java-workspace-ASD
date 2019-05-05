251473687acaf83093f10c146f5d59b4a404d6dd
Mirco <Mirco@DESKTOP-A55DTAV>
2019-05-05 19:18:13 +0200

    tip

---
+++ ASD Es13 Coda/src/Coda/Coda.java
@@ -0,0 +1,10 @@
+package Coda;
+
+public interface Coda {
+
+	public boolean isEmpty();
+	public void enqueue(Object elem);
+	public void dequeue();
+	public Object first();
+
+}
---
+++ ASD Es15 Dizionario/src/Dizionario/Dizionario.java
@@ -0,0 +1,9 @@
+package Dizionario;
+
+public interface Dizionario<T> {	
+	public void insert(T elem,Comparable key);
+	public void delete(Comparable key);
+	public T search(Comparable key);
+	
+
+}
